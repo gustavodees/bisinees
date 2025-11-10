f66b785af5cb19a9094be8544a739155d29eefcb
ChocolateKest <gustavoemartins@gmail.com>
2025-11-09 21:36:46 -0300

    Service layer, component registration

---
+++ src/main/java/com/startup/bisiness/services/UserService.java
@@ -0,0 +1,31 @@
+package com.startup.bisiness.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.startup.bisiness.entities.User;
+import com.startup.bisiness.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+
+}
