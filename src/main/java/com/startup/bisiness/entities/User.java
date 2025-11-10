3171c0f6d3f3e50690465e9ec416615dd32c932b
ChocolateKest <gustavoemartins@gmail.com>
2025-11-09 21:05:38 -0300

    H2 database, test profile, JPA

--- src/main/java/com/startup/bisiness/entities/User.java
+++ src/main/java/com/startup/bisiness/entities/User.java
@@ -3,9 +3,21 @@ package com.startup.bisiness.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -16,8 +28,6 @@ public class User implements Serializable{
 	public User() {
 		
 	}
-	
-	
 	
 	
 	public User(Long id, String name, String email, String phone, String password) {
