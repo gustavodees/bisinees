c0f7d29a995d8511767eb3531fe4a57ea0738842
ChocolateKest <gustavoemartins@gmail.com>
2025-12-02 22:29:10 -0300

    Product

--- src/main/java/com/startup/bisiness/config/TestConfig.java
+++ src/main/java/com/startup/bisiness/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.startup.bisiness.entities.Category;
 import com.startup.bisiness.entities.Order;
+import com.startup.bisiness.entities.Product;
 import com.startup.bisiness.entities.User;
 import com.startup.bisiness.entities.enums.OrderStatus;
 import com.startup.bisiness.repositories.CategoryRepository;
 import com.startup.bisiness.repositories.OrderRepository;
+import com.startup.bisiness.repositories.ProductRepository;
 import com.startup.bisiness.repositories.UserRepository;
 
 @Configuration
@@ -29,12 +31,23 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private CategoryRepository categoryRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		
