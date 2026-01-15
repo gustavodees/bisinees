eadb2a9a1446843e3f1c2375618b8831fc318cfe
ChocolateKest <gustavoemartins@gmail.com>
2026-01-14 22:07:59 -0300

    Payment, one-to-one association

--- src/main/java/com/startup/bisiness/entities/Order.java
+++ src/main/java/com/startup/bisiness/entities/Order.java
@@ -7,9 +7,9 @@ import java.util.Objects;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.startup.bisiness.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 
@@ -43,6 +44,8 @@ public class Order implements Serializable{
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();	
 	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 		
@@ -107,6 +110,21 @@ public class Order implements Serializable{
 		return items;
 	}
 	
+
+	
+	
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+
 
 	@Override
 	public int hashCode() {
